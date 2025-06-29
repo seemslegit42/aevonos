f92f041ad05d09d9bc10d96e741c8d680839d9d2
Bryan Marlow <brylow@aevon.cloud>
2025-06-29 23:18:53 +0000

    Beep avatar

--- src/components/beep-avatar.tsx
+++ src/components/beep-avatar.tsx
@@ -1,1 +1,18 @@
-// This file has been decommissioned as part of the comprehensive overhaul.
+'use client';
+
+import React from 'react';
+import { Avatar, AvatarFallback } from '@/components/ui/avatar';
+import { BeepIcon } from '@/components/icons/BeepIcon';
+import { cn } from '@/lib/utils';
+
+interface BeepAvatarProps extends React.HTMLAttributes<HTMLSpanElement> {}
+
+export function BeepAvatar({ className, ...props }: BeepAvatarProps) {
+  return (
+    <Avatar className={cn('bg-primary/10', className)} {...props}>
+      <AvatarFallback className="bg-transparent p-1">
+        <BeepIcon className="h-full w-full text-primary" />
+      </AvatarFallback>
+    </Avatar>
+  );
+}
