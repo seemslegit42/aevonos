4a678f15b5e8364f18a9576efcb0b90e61f75d24
Bryan Marlow <brylow@aevon.cloud>
2025-07-01 21:27:25 +0000

    ΛΞVON OS: Core Subsystems - The ΛΞVON Armory Marketplace 1. Introduction

---
+++ src/config/chaos-cards.ts
@@ -0,0 +1,4 @@
+
+// This file has been decommissioned as part of a comprehensive project cleanup.
+// It has been replaced by the unified `config/artifacts.ts` manifest.
+// This file can be safely removed in a future refactor.
