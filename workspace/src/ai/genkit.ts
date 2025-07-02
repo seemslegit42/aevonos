138bc3c5315880399363d7ac9857f1118a10d02b
Bryan Marlow <brylow@aevon.cloud>
2025-07-02 05:02:40 +0000

    I ALSO HAD TO MOVE ALL THE FUCKING DOCS BECAUSE YOU STUPID FUCKING IDIOT

--- workspace/src/ai/genkit.ts
+++
@@ -1,4 +0,0 @@
-
-// This file is obsolete after project consolidation and has been marked for deletion.
-// It has been replaced by /src/ai/genkit.ts.
-// Its presence could cause incorrect imports.
