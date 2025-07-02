e9bd2f292fc9d85910b607bd69b38053891ff2d9
Bryan Marlow <brylow@aevon.cloud>
2025-07-02 00:11:35 +0000

    ITS NOT NEXTAUTH YOU FUCKING MORON ITS AUTHJS

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,13 +1,9 @@
 
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
-
-// Initialize NextAuth with the edge-compatible config.
-// The `auth` function from this initialization is what the middleware will use.
-// It relies only on the JWT for session data, not the database adapter.
-export default NextAuth(authConfig).auth;
-
+import { auth } from '@/auth';
+ 
+export default auth;
+ 
 export const config = {
   // Match all routes except for specific public paths and internal Next.js assets
   matcher: ['/((?!api/auth|login|register|pricing|subscribe|validator|_next/static|_next/image|favicon.ico|logo.png|logo-light-purple.png).*)'],
-}
+};
