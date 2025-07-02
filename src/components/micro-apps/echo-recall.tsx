c486cdf34175ec532b209b8ee306d135215c8166
Bryan Marlow <brylow@aevon.cloud>
2025-07-02 06:36:04 +0000

    you must move all redundant files into a specific directory, its insanty

--- src/components/micro-apps/echo-recall.tsx
+++ src/components/micro-apps/echo-recall.tsx
@@ -1,4 +1,3 @@
 
-// This file has been decommissioned as part of a comprehensive project cleanup.
-// It was a duplicate and/or obsolete file.
-// This file can be safely removed in a future refactor.
+// This file has been moved to /src/decommissioned/echo-recall.tsx for archival.
+// This file can be safely deleted.
