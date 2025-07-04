b45e2c3ecc5d7b1c75ea6762ed9c9d3702714917
Bryan Marlow <brylow@aevon.cloud>
2025-07-04 21:23:24 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@firebase/app', '@firebase/auth'],
   async headers() {
     return [
       {
