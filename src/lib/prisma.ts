8bc33cad56d258afcc5d8c106366624ebce00e63
Bryan Marlow <brylow@aevon.cloud>
2025-07-04 06:51:02 +0000

    NO. REMOVE NEON. IT IS NOT FUCKING HOSTED ON NEON. IT IS HOSTED ON PRISM

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,16 @@
 
-import { PrismaClient } from "@prisma/client";
-import { PrismaNeon } from '@prisma/adapter-neon'
-import { Pool } from '@neondatabase/serverless'
-import ws from 'ws'
- 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient | undefined };
- 
-const neon = new Pool({ connectionString: process.env.DATABASE_URL })
-const adapter = new PrismaNeon(neon, { ws })
+import { PrismaClient } from '@prisma/client';
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter });
- 
-if (process.env.NODE_ENV !== "production") {
-    globalForPrisma.prisma = prisma;
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
 }
- 
+
 export default prisma;
